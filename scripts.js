5746278ea726cfdb3180e16e6af15d066660e508
Edyta Zawada <edyta.zawada88@gmail.com>
2017-08-06 13:00:14 +0200

    new reposytory

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var a= prompt("Wpisz wartość a");
+	b= prompt("Wpisz wartość b");
+	value=(a * a) + (2 * a * b) - (b * b);
+
+console.log(value);
+
+if (value>0) {
+  console.log('Wynik dodatni')
+} else if (value<0) {
+  console.log('Wynik ujemny')
+} else {
+  console.log('Wynik jest równy zero')
+}
